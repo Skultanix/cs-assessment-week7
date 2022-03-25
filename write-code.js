00bec2b39f9e2a8439d0f8611627bf238604332b
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-03-25 11:26:53 -0600

    Estimates of runtime complexity added in comments.

--- write-code.js
+++ write-code.js
@@ -12,22 +12,28 @@ function sumZero(arr) {
   return baseCond
 }
 console.log(sumZero(testArr))
+
+//Runtime complexity = O(n)
+
 ///Unique Characters
 let uniqCheck = (aWord) => {
-    //make new set from the initial string
     const aWordSet = new Set(aWord)
-    //make an array from a the initial string using spread
     let aWordArr = [...aWord]
-    //compare the length of the array to the length of the set and return the value
     return aWordSet.size === aWordArr.length
   } 
 console.log(uniqCheck('moon'))
+
+//Runtime complexity = O(1)
+
 ///Pangram Sentence
 function isPangram(str) {
     let alphabetCheck = /([a-z])(?!.*\1)/gi
     return(str.match(alphabetCheck)).length === 26
 }
 console.log(isPangram("The quick brown fox jumps over the lazy dog!"))
+
+//Runtime complexity = O(n)
+
 ///Longest Word
 function longestWord(arr) {
     let longest = 0
@@ -39,3 +45,5 @@ function longestWord(arr) {
     return longest
 }
 console.log(longestWord(["radial", "radical", "elephant", "Wilbur"]))
+
+//Runtime complexity = O(n)
